35fa8d343050b387bc99cddc6702033e9c1187a3
Danielle Sweeny <dsweeny1@gmail.com>
2022-03-29 20:27:57 -0600

    Update variables.js

---
+++ variables.js
@@ -0,0 +1,10 @@
+var string1 = "This is string 1";
+var string2 = "This is string 2";
+var string3 = "This is string 3";
+
+var number1 = 1
+var number2 = 2
+var number3 = 3
+
+var boolean1 = true
+var boolean2 = false
